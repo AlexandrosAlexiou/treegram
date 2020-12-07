641c11adafa84eae61dadec63a58a552c909097b
Alexandros Alexiou <AlexandrosAlexiou@users.noreply.github.com>
2020-12-07 22:31:31 +0200

    Remove redundant params permissions in follows_controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,8 +5,6 @@ class FollowsController < ApplicationController
   end
 
   def create
-    # follower_id = session[:user_id]
-    # followed_id = params[:user_id]
     @follower = User.find(session[:user_id])
     @followed = User.find(params[:user_id])
     logger.debug "Follower: #{@follower.id}, Following: #{@followed.id}"
@@ -14,11 +12,6 @@ class FollowsController < ApplicationController
       flash[:notice]= "You are now following #{@followed.email}"
       redirect_to users_path
     end
-  end
-
-  private
-  def follow_params
-    params.require(:follow).permit( :follower_id, :followed_id)
   end
 
 end
