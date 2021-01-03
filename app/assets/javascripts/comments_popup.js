71980e00f6f31fc58211932bdb4e17c5ebcb1e52
Alexandros Alexiou <AlexandrosAlexiou@users.noreply.github.com>
2021-01-03 20:06:49 +0200

    Minor improvements

--- app/assets/javascripts/comments_popup.js
+++ app/assets/javascripts/comments_popup.js
@@ -1,33 +1,31 @@
-let CommentsPopup = {
+var commentPopup = {
     setup: function() {
         // add hidden 'div' to end of page to display popup:
-        let popupDiv = $('<div id="photoComments"></div>');
+        var popupDiv = $('<div id="photoComments"></div>');
         popupDiv.hide().appendTo($('body'));
-        $(document).on('click', '#slide ', CommentsPopup.getPhotoComments);
+        $(document).on('click', '#slide', commentPopup.getPhotoInfo);
     }
-    ,getPhotoComments: function() {
-        //console.log($(this).attr('href'));
+    ,getPhotoInfo: function() {
         $.ajax({type: 'GET',
             url: $(this).attr('href'),
             timeout: 5000,
-            success: CommentsPopup.showPhotoComments,
+            success: commentPopup.showPhotoInfo,
             error: function(xhrObj, textStatus, exception) { alert('Error!'); }
             // 'success' and 'error' functions will be passed 3 args
         });
         return false;
     }
-    ,showPhotoComments: function(data, requestStatus, xhrObject) {
-        console.log("showPhotoComments called")
+    ,showPhotoInfo: function(data, requestStatus, xhrObject) {
         // center a floater 1/2 as wide and 1/4 as tall as screen
-        let oneFourth = Math.ceil($(window).width() / 3);
+        var oneFourth = Math.ceil($(window).width() / 4);
         $('#photoComments').css({'left': oneFourth,  'width': 2*oneFourth, 'top': 250}).html(data).show();
         // make the Close link in the hidden element work
-        $('#closeLink').click(CommentsPopup.hidePhotoComments);
-        return false;  // prevent default link action
+        $('#closeLink').click(commentPopup.hidePhotoInfo);
+        return false ;  // prevent default link action
     }
-    ,hidePhotoComments: function() {
+    ,hidePhotoInfo: function() {
         $('#photoComments').hide();
-        return false ;
+        return false;
     }
 };
-$(CommentsPopup.setup);
+$(commentPopup.setup);
